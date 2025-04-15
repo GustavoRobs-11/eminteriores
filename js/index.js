12e65f2da041a921b2fe5d82488aaeb2a525e95b
Gustavo Robson <gustavo.silva510@fatec.sp.gov.br>
2025-04-15 20:50:19 -0300

    HomeAndSobre

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 function WPP(){
     window.open("https://wa.me/message/XVAZW2UCDBJVL1");
 }
+function Insta(){
+    window.open("https://www.instagram.com/e.m.interiores/")
+}
+function Tiktok(){
+    window.open("https://www.tiktok.com/@e.m.interiores?lang=pt-BR")
+}
+function Sobre(){
+    window.location.href = "/pages/sobre.html";
+}
+function Home(){
+    window.location.href = "/index.html";
+}
